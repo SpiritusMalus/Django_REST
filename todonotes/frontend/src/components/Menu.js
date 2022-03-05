7eec39f6713cdb6f17f86ad7ece808306cfd5ad1
Евгений <80955062+SpiritusMalus@users.noreply.github.com>
2022-03-05 12:45:04 +0300

    Lesson#2, Menu/Footer/Users (#1)

---
+++ todonotes/frontend/src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const MenuItems = () => {
+  return (
+      <div className="btn-group">
+        <a href="#" className="btn btn-primary" aria-current="page">Все пользователи</a>
+        <a href="#" className="btn btn-primary">Проект</a>
+        <a href="#" className="btn btn-primary">TODO</a>
+      </div>
+  )
+}
+
+export default MenuItems
