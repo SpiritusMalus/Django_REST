e0240145a5350396dd1a7965640836791020a05c
SpiritusMalus <Leos2111@gmail.com>
2022-03-30 22:52:17 +0300

    Fixed Lesson#3

--- todonotes/TODO/models.py
+++ todonotes/TODO/models.py
@@ -18,4 +18,7 @@ class ToDo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField()
+    users = models.ManyToManyField(Users)
 
+    def __str__(self):
+        return self.users
