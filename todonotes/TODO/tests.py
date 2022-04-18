841e2ce6df9da30a7aa9644f7317aeb8c43974ab
SpiritusMalus <Leos2111@gmail.com>
2022-04-18 21:56:35 +0300

    Lesson#8 last tests

--- todonotes/TODO/tests.py
+++ todonotes/TODO/tests.py
@@ -1,8 +1,7 @@
 from mixer.backend.django import mixer
 from rest_framework import status
 from rest_framework.test import APITestCase
-
-from .models import Project, ToDo
+from .models import Project
 from usersapp.models import Users
 
 
@@ -15,8 +14,7 @@ class TestProjectModelViewSet(APITestCase):
 
         self.admin = Users.objects.create_superuser(username=self.name, password=self.password, email=self.email)
         self.user_create = Users.objects.create(username='Malum', password='y6661342Ll', email='user_u@user.ru')
-
-        self.data = {'name': 'Mortys', 'urls_rep': 'http://127.0.0.1:8000/api/users/', 'users': [self.user_create.id]}
+        self.data = {'name': 'Mortys', 'urls_rep': 'http://127.0.0.1:8000/api/project/'}
         self.url = '/api/project/'
 
     def test_get_list(self):
@@ -26,21 +24,19 @@ class TestProjectModelViewSet(APITestCase):
 
     def test_create_project(self):
         project = Project.objects.create(**self.data)
-        # user = Users.objects.first()
-        # print(user.id)
-        # project.users.add(user.id)
+        project.users.set([self.user_create.id])
         self.client.login(username=self.name, password=self.password)
-        response = self.client.post(self.url, self.data, format='json')
+        response = self.client.post(self.url, {**self.data, 'users': [project.id]}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.client.logout()
+
+    def test_put_mixer(self):
+        project = mixer.blend(Project)
+        self.client.login(username=self.name, password=self.password)
+        response = self.client.put(f'{self.url}{project.id}/', {'name': 'ChangeName', 'urls_rep': 'http://127.0.0.1:8000/api/', 'users': [project.id]})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.client.logout()
 
-
-    # def test_put_mixer(self):
-    #
-    #     project = mixer.blend(Project, name='ChangeName')
-    #     self.client.login(username=self.name, password=self.password)
-    #     response = self.client.put(f'{self.url}{project.id}/')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
     def tearDown(self) -> None:
         pass
+
